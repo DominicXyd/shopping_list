ce0992dfc98bc0035dbfdd640ecebd6061324cfe
DominicXyd <dxydakis@unomaha.edu>
2026-03-11 20:13:51 -0500

    Update urls.py

--- shopping/urls.py
+++ shopping/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.shopping_list, name='shopping_list'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
+    path('toggle/<int:item_id>/', views.toggle_purchased, name='toggle_purchased'),
+]
